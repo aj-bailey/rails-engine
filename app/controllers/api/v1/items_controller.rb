0d5cfd775d0f9ad3160cb794962fbbb17c64f1df
Adam Bailey <ab67319@gmail.com>
2023-03-28 13:21:37 -0600

    feat: create item udpate request api functionality

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -17,6 +17,14 @@ class Api::V1::ItemsController < ApplicationController
     render status: 204
   end
 
+  def update
+    item = Item.find(params[:id])
+
+    item.update!(item_params)
+
+    render json: ItemSerializer.new(item)
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
