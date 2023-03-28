4d027e667126f4153866caa335083b0f2bd8ac26
Adam Bailey <ab67319@gmail.com>
2023-03-28 11:29:22 -0600

    feat: create item create request api functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActiveRecord::RecordInvalid, with: :render_400
 
   def render_404
     render plain: "404 Not Found", status: 404
+  end
+
+  def render_400
+    render plain: "400 Bad Request", status: 400
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
+
+  validates_presence_of :name, :description, :unit_price, :merchant_id
+  validates_numericality_of :unit_price
 end
