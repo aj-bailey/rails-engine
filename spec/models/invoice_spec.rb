f24134a65e484c5d8e5b7db8d867372a78806bcb
Adam Bailey <ab67319@gmail.com>
2023-03-29 08:38:56 -0600

    test: create model test for ::single_item_invoices

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,4 +7,31 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :invoice_items }
     it { should have_many :transactions }
   end
+
+  describe "::class methods" do
+    describe "::single_item_invoices" do
+      it "returns a list of invoices that only have the associated item" do
+        merchant = create(:merchant)
+
+        item_1 = create(:item, merchant: merchant)
+        item_2 = create(:item, merchant: merchant)
+
+        invoice_1 = create(:invoice, merchant: merchant)
+        invoice_2 = create(:invoice, merchant: merchant)
+        invoice_3 = create(:invoice, merchant: merchant)
+
+        invoice_4 = create(:invoice, merchant: merchant)
+
+        create(:invoice_item, item: item_1, invoice: invoice_1)
+        create(:invoice_item, item: item_1, invoice: invoice_2)
+        create(:invoice_item, item: item_1, invoice: invoice_3)
+        create(:invoice_item, item: item_1, invoice: invoice_4)
+        create(:invoice_item, item: item_2, invoice: invoice_4)
+
+        expected_results = [invoice_1, invoice_2, invoice_3]
+
+        expect(item_1.invoices.single_item_invoices).to eq(expected_results)
+      end
+    end
+  end
 end
