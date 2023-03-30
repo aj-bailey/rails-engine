8b4815c908b008a64b32b1f4fb9c5f2e0fafdc6e
Adam Bailey <ab67319@gmail.com>
2023-03-29 18:49:38 -0600

    test: create model method test for ::find_merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many :invoices }
   end
+
+  describe '::find_merchant' do
+    before(:each) do
+      @turing = create(:merchant, name: "Turing")
+      @ring_world = create(:merchant, name: "Ring World")
+      @thing_world = create(:merchant, name: "Thing World")
+    end
+
+    it 'returns the first merchant found from case-insensitive alphabetical order results from case-insensitive keyword' do
+      expect(Merchant.find_merchant("ring")).to eq(@ring_world)
+      expect(Merchant.find_merchant("RiNg")).to eq(@ring_world)
+    end
+
+    it 'returns nil if no matches' do
+      expect(Merchant.find_merchant("beer")).to eq(nil)
+    end
+  end
 end
